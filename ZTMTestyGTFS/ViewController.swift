9bf7e9c3cfec1966910fc0a25be175667e5a3522
Karol Struniawski <charlie.warhammer@gmail.com>
2019-09-02 15:22:46 +0200

    Initial Commit

---
+++ ZTMTestyGTFS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZTMTestyGTFS
+//
+//  Created by Karol Struniawski on 02/09/2019.
+//  Copyright © 2019 Karol Struniawski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
